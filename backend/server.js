552336d1c6586f5414eee8a7963c773ca8cae544
Prakhar Mishra <mishra.prakhar436@gmail.com>
2024-06-08 17:48:53 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve(); // this path will be coming from nodeJS
 
 // app.get("/",(req,res)=>{
 //     // root route http://localhost:5000/
@@ -37,11 +38,20 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); // to access the cookies
 
-app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
-server.listen(PORT,()=>{
-    connectToMongoDB();
-    console.log(`Server is running on port ${PORT}`)
+app.use(express.static(path.join(__dirname,"/frontend/dist"))); // serve static files such as html css javascript images sounds files
+                                // we are going into the dirname/frontend/dist folder
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html")); // run our frontend from server as well
+})
+
+// any route other than the main three, can you render the index.html in frontend/dist
+
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
 });
